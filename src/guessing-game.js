3a0b1e79d3091aa1781d53fc5d5d18365215112a
Резанович Игорь <Rezanovich@jukola.office>
2023-02-12 23:21:42 +0300

    feat: implement task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,67 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  minRange = "";
+  maxRange = "";
+  candidate = "";
 
+  setRange(min, max) {
+    this.minRange = min;
+    this.maxRange = max;
+    this.candidate = "";
+  }
+
+  guess() {
+    if (this.candidate === "") {
+      this.candidate = Math.round((this.minRange + this.maxRange) / 2);
     }
+    // console.log(this.candidate, (this.minRange + this.maxRange) / 2);
+    return this.candidate;
+  }
 
-    guess() {
-
+  lower() {
+    if (this.candidate !== "") {
+      this.maxRange = this.candidate;
+      this.candidate = "";
     }
+    // console.log("lower");
+  }
 
-    lower() {
-
+  greater() {
+    if (this.candidate !== "") {
+      this.minRange = this.candidate;
+      this.candidate = "";
     }
-
-    greater() {
-
-    }
+    // console.log("greater");
+  }
 }
 
 module.exports = GuessingGame;
+
+const number = 409;
+const game = new GuessingGame();
+game.setRange(0, 4048);
+
+let result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
